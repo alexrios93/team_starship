9cf82800261084cc3d07d5c2bdf3c15f8dfe7d03
alexrios93 <alejandro.c.rios01@utrgv.edu>
2017-03-23 21:49:12 -0500

    Created player ship and added basic controls
    Created player ship, added basic movement controls and shooting featuers

---
+++ Assets/ShipMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour {
+
+    public float movementSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += (new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0)) * movementSpeed * Time.deltaTime;
+	}
+}
