44c43e8ab6dc6ec5be4a106be3f1bd17955b1af0
alexrios93 <alejandro.c.rios01@utrgv.edu>
2017-04-17 15:15:23 -0500

    Player MiniMap Implemented

---
+++ Assets/Scripts/PlayerScripts/MiniMapFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapFollow : MonoBehaviour {
+    public GameObject _player;
+    public float distance;
+
+    // Use this for initialization
+    void Start () {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(_player.transform.position.x, _player.transform.position.y + distance, _player.transform.position.z);
+    }
+}
