236d3f1e74532a0733e7031fa44f8ce651bf8179
alexrios93 <alejandro.c.rios01@utrgv.edu>
2017-04-25 20:14:33 -0500

    InfoMenu added to StartMenu Scene
    InfoMenu added to StartMenu scene to give players context about the game
    objectives.

--- Assets/Scripts/StartMenuScripts/MenuScript.cs
+++ Assets/Scripts/StartMenuScripts/MenuScript.cs
@@ -5,29 +5,53 @@ using UnityEngine.SceneManagement;
 public class MenuScript : MonoBehaviour {
 
     public Canvas QuitMenu;
+    public Canvas InfoMenu;
     public Button playText;
     public Button exitText;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        InfoMenu = InfoMenu.GetComponent<Canvas>();
         QuitMenu = QuitMenu.GetComponent<Canvas>();
         playText = playText.GetComponent<Button>();
         exitText = exitText.GetComponent<Button>();
         QuitMenu.enabled = false;
+        InfoMenu.enabled = false;
     }
 
     public void ExitPress()
     {
         QuitMenu.enabled = true;
+        InfoMenu.enabled = false;
         playText.enabled = false;
         exitText.enabled = false;
+
+        if(Input.GetButtonDown("Start Button")  || Input.GetButtonDown("A Button")) {
+            ExitGame();
+        }
+        else if(Input.GetButtonDown("Back Button")  || Input.GetButtonDown("B Button")) {
+            NoPress();
+        }
     }
 
     public void NoPress()
     {
         QuitMenu.enabled = false;
+        InfoMenu.enabled = false;
         playText.enabled = true;
         exitText.enabled = true;
+    }
+
+    public void PlayPress() //Loads up the InfoMenu
+    {
+        QuitMenu.enabled = false;
+        InfoMenu.enabled = true;
+        playText.enabled = false;
+        exitText.enabled = false;
+
+        if(Input.GetButtonDown("A Button")) {
+            StartScene();
+        }
     }
 
     public void StartScene()
@@ -43,8 +67,14 @@ public class MenuScript : MonoBehaviour {
     // Joystick Control Start
     void Update()
     {
-        if(Input.GetButtonDown("Start Button")  || Input.GetButtonDown("A Button")) {
-            StartScene();
+        // if(Input.GetButtonDown("Start Button")  || Input.GetButtonDown("A Button")) {
+        //     StartScene();
+        // }
+        if(Input.GetButtonDown("Start Button")) {
+            PlayPress();
+        }
+        else if(Input.GetButtonDown("Back Button")  || Input.GetButtonDown("B Button")) {
+            ExitPress();
         }
     }
 }
