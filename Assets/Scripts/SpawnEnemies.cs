08ee964c0bf36c41cd86a50c3101cd38488dfbc3
e-gonzalez01 <elizardo.gonzalez01@utrgv.edu>
2017-04-27 09:04:21 -0500

    Mothership Spawns Enemies
    -- Features Added --
    
    Our mothership can now spawn enemy  ships in timed intrevals.
    
    The newly spawned ships will also seek and destroy the two satellites in
    the scene.

---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SpawnEnemies : MonoBehaviour {
+    public GameObject Enemy;
+    public GameObject Target;
+    public GameObject TargetAlternative;
+    public GameObject SpawnLocation;
+    
+    private float SpawnDelay = 5.0f;
+    private float LastSpawn = 0.0f;
+
+    private GameObject EnemyTarget;
+    private GameObject EnemyTargetAlternative;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (SpawnDelay < (Time.time - LastSpawn))
+        {
+            LastSpawn = Time.time;
+            //// SELECT TARGET ////
+            if ((UnityEngine.Random.Range(0, 2) == 0) & (Target != null))
+            {
+                EnemyTarget = Target;
+                EnemyTargetAlternative = TargetAlternative;
+            }
+            else if (TargetAlternative != null)
+            {
+                EnemyTarget = TargetAlternative;
+                EnemyTargetAlternative = Target;
+            }
+            else
+            {
+                return;
+            }
+
+            //// SPAWN ENEMY UNIT ////
+            Vector3 RelativePos = Target.transform.position - transform.position;
+            GameObject EnemyShip = Instantiate(Enemy, SpawnLocation.transform.position, Quaternion.LookRotation(RelativePos));
+
+            //// DESIGNATE TARGETS ////
+            EnemyShip.GetComponent<SeekAndDestroy>().Target = EnemyTarget;
+            EnemyShip.GetComponent<SeekAndDestroy>().TargetAlternative = EnemyTargetAlternative;
+        }   
+    }
+}
