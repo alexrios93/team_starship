d9b67c493e91b6bf5ab591fd620ffce0f4aab628
e-gonzalez01 <elizardo.gonzalez01@utrgv.edu>
2017-05-04 23:03:06 -0500

    The Motherships portal now opens!
    -- Features Added --
    
    The mothership will now open its portal to spawn enemies and closes the
    protal to recharge.

--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -8,9 +8,18 @@ public class SpawnEnemies : MonoBehaviour {
     public GameObject Target;
     public GameObject TargetAlternative;
     public GameObject SpawnLocation;
+    public GameObject Portal;
+
     
-    private float SpawnDelay = 5.0f;
-    private float LastSpawn = 0.0f;
+
+    private float PortalOpenDelay = 30.0f;
+    private float LastPortalOpen = -25.0f;
+
+    private float SpawnDelay = 30.0f;
+    private float LastSpawn = -20.0f;
+
+    private float PortalCloseDelay = 30.0f;
+    private float LastPortalClose = -15.0f;
 
     private GameObject EnemyTarget;
     private GameObject EnemyTargetAlternative;
@@ -18,42 +27,70 @@ public class SpawnEnemies : MonoBehaviour {
     public bool status = false;  // Allows for Pause & Play in ControlLayout
 
     // Use this for initialization
-    void Start () {
-		
-	}
+    void Start ()
+    {
+        Portal.GetComponent<ParticleSystem>().Stop();
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (status)  // Needed for ControlLayout
         {
+            if (PortalOpenDelay < (Time.time - LastPortalOpen))
+            {
+                OpenPortal();
+            }
+
             if (SpawnDelay < (Time.time - LastSpawn))
             {
-                LastSpawn = Time.time;
-                //// SELECT TARGET ////
-                if ((UnityEngine.Random.Range(0, 2) == 0) & (Target != null))
-                {
-                    EnemyTarget = Target;
-                    EnemyTargetAlternative = TargetAlternative;
-                }
-                else if (TargetAlternative != null)
-                {
-                    EnemyTarget = TargetAlternative;
-                    EnemyTargetAlternative = Target;
-                }
-                else
-                {
-                    return;
-                }
+                SpawnEnemyShips();
+            }
 
-                //// SPAWN ENEMY UNIT ////
-                Vector3 RelativePos = Target.transform.position - transform.position;
-                GameObject EnemyShip = Instantiate(Enemy, SpawnLocation.transform.position, Quaternion.LookRotation(RelativePos));
-
-                //// DESIGNATE TARGETS ////
-                EnemyShip.GetComponent<SeekAndDestroy>().Target = EnemyTarget;
-                EnemyShip.GetComponent<SeekAndDestroy>().TargetAlternative = EnemyTargetAlternative;
+            if (PortalCloseDelay < (Time.time - LastPortalClose))
+            {
+                ClosePortal();
             }
         }
+    }
+
+    void OpenPortal()
+    {
+        LastPortalOpen = Time.time;
+        Portal.GetComponent<ParticleSystem>().Play();
+    }
+
+    void SpawnEnemyShips()
+    {
+        LastSpawn = Time.time;
+        //// SELECT TARGET ////
+        if ((UnityEngine.Random.Range(0, 2) == 0) & (Target != null))
+        {
+            EnemyTarget = Target;
+            EnemyTargetAlternative = TargetAlternative;
+        }
+        else if (TargetAlternative != null)
+        {
+            EnemyTarget = TargetAlternative;
+            EnemyTargetAlternative = Target;
+        }
+        else
+        {
+            return;
+        }
+
+        //// SPAWN ENEMY UNIT ////
+        Vector3 RelativePos = Target.transform.position - transform.position;
+        GameObject EnemyShip = Instantiate(Enemy, SpawnLocation.transform.position, Quaternion.LookRotation(RelativePos));
+
+        //// DESIGNATE TARGETS ////
+        EnemyShip.GetComponent<SeekAndDestroy>().Target = EnemyTarget;
+        EnemyShip.GetComponent<SeekAndDestroy>().TargetAlternative = EnemyTargetAlternative;
+    }
+
+    void ClosePortal()
+    {
+        LastPortalClose = Time.time;
+        Portal.GetComponent<ParticleSystem>().Stop();
     }
 }
